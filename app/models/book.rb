18ee6bebc8aee147fa466b66aa65801a2e98ed25
sakatashi <shiori.1002sakata@gmail.com>
2022-09-28 12:56:10 +0000

    Add:enum

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user
-    has_many :favorites, dependent: :destroy
-    has_many :book_comments,dependent: :destroy
-    has_one_attached :image
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments,dependent: :destroy
+  
+  has_one_attached :image
+  
+  enum status: { published: 0, draft: 1 }
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
